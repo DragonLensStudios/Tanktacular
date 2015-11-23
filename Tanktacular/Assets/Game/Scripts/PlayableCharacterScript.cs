dc18d211a201bbd59fa4a2bfa1fecf0c7329f4e4
Jeffery Miller <Mony@dragonlensstudios.com>
2015-11-23 06:03:15 -0700

    Added Scripts, Added Prefabs, Edited Scripts
    Added PlayableCharacterScript which handles the script on a playable
    prefab.
    
    Added CharacterButtonScript that hapndles Button Setup for character
    buttons.
    
    tweaked Tankhealth script removed old colors and just replaced with
    Gradient.
    
    Added Character Prefabs for Tanks for testing.

---
+++ Tanktacular/Assets/Game/Scripts/PlayableCharacterScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayableCharacterScript : MonoBehaviour
+{
+    public string Name;
+    public Sprite Portrit;
+    public bool Unlocked;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (Unlocked == false)
+        {
+            Name = "???";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Tanktacular/Assets/Game/Scripts/UI/CharacterButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CharacterButtonScript : MonoBehaviour
+{
+    public GameObject m_CharacterPrefab;
+    private PlayableCharacterScript m_CharacterScript;
+    private Text m_CharacterNameText;
+    private Image m_CharacterImage;
+
+    void Awake()
+    {
+        m_CharacterScript = m_CharacterPrefab.GetComponent<PlayableCharacterScript>();
+        m_CharacterNameText = GetComponentInChildren<Text>();
+        m_CharacterImage = transform.GetChild(0).GetComponent<Image>();
+        
+    }
+
+    void OnEnable()
+    {
+        if (m_CharacterScript.Unlocked == false)
+        {
+            m_CharacterNameText.text = "???";
+        }
+        else
+        {
+            m_CharacterNameText.text = m_CharacterScript.Name;
+            m_CharacterImage.sprite = m_CharacterScript.Portrit;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
